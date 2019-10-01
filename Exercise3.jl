5f6eba42051bf7053903712b5f53db25da61eb7f
Randall <wsr2@princeton.edu>
2019-10-01 17:07:22 -0400

    1 A. e.

--- Exercise3.jl
+++ Exercise3.jl
@@ -24,7 +24,23 @@ function extractimagechannels(imagename)
 
 end
 
+#A. c/d.
+println("Reset with git reset --mixed")
 
+#A. e.
+image2 = zeros(360,640,3)
+image2[:,:,1]=image[:,:,2] #1 was red, now green
+image2[:,:,2]=image[:,:,3] #2 was green, now blue
+image2[:,:,3]=image[:,:,1] #3 was blue, now red
+
+subplot(1,2,1)
+imshow(image)
+title("El-Capitan Image")
+axis("off")
+subplot(1,2,2)
+imshow(image2)
+title("El-Capitan Altered Image")
+axis("off")
 
 
 #just hit save button on image to push into gitkraken
